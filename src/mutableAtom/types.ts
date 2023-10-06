f95519fd227a4aabea849aed86a3b50ce360fb6f
daishi <daishi@axlight.com>
2023-10-06 11:11:19 +0900

    refactor

--- src/mutableAtom/types.ts
+++ src/mutableAtom/types.ts
@@ -2,12 +2,10 @@ export type Wrapped<T> = { value: T }
 
 type ProxyFn<T> = (obj: Wrapped<T>) => Wrapped<T>
 
-type PromiseOrValue<T> = Promise<T> | T
-
 export type Store<Value> = {
-  proxyState: ProxyState<Value> | null
+  proxyState: ProxyState<Value>
   getValue: () => Value
-  setValue: (value: Value) => PromiseOrValue<void>
+  setValue: (value: Value) => void
 }
 
 export type Options<T> = {
